984aeeed81e28cc5d2e5a787973eab91eae711c8
Atharva-001-cyber <atharvranjans65@gmail.com>
2026-07-24 20:52:44 +0530

    Time: 1 ms (99.96%), Space: 47.1 MB (13.56%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
