cf98f131c796003270f02f85eb25b06ffcdbafef
Atharva-001-cyber <atharvranjans65@gmail.com>
2026-08-04 23:41:07 +0530

    Time: 13 ms (42.62%), Space: 47.3 MB (89.04%) - LeetHub

---
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minInsertions(String s) {
+        int ans = 0;
+        int need = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                need += 2;
+
+                if (need % 2 == 1) {
+                    ans++;
+                    need--;
+                }
+            } else {
+                need--;
+
+                if (need == -1) {
+                    ans++;
+                    need = 1;
+                }
+            }
+        }
+
+        return ans + need;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
