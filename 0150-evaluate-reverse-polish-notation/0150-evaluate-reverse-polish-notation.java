5e9084d13a5b96fd2486719c5fbafd278c56d83a
Atharva-001-cyber <atharvranjans65@gmail.com>
2026-08-07 12:21:24 +0530

    Time: 6 ms (87.22%), Space: 45.4 MB (29.23%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,39 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < tokens.length; i++) {
-
-            String s = tokens[i];
-
-            if (s.equals("+")) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(a + b);
-
-            } else if (s.equals("-")) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(a - b);
-
-            } else if (s.equals("*")) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(a * b);
-
-            } else if (s.equals("/")) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(a / b);
-
-            } else {
-                stack.push(Integer.parseInt(s));
-            }
-        }
-
-        return stack.pop();
+      Stack<Integer> stack = new Stack<>();
+      for(int i = 0;i < tokens.length; i++){
+        String s = tokens[i];
+        if(s.equals("+")){
+            int b = stack.pop();
+            int a = stack.pop();
+            stack.push(a+b);
+        }else if(s.equals("-")){
+            int b = stack.pop();
+            int a = stack.pop();
+            stack.push(a-b);
+        }else if(s.equals("*")){
+            int b = stack.pop();
+            int a = stack.pop();
+            stack.push(a*b);
+        }else if(s.equals("/")){
+            int b = stack.pop();
+            int a = stack.pop();
+            stack.push(a/b);
+      }else{
+        stack.push(Integer.parseInt(s));
+      }
     }
+    return stack.peek(); // stack.pop() can also be written instead of stack.peek()
+}
 }
 
 // Synced seamlessly with LeetHub Pro
