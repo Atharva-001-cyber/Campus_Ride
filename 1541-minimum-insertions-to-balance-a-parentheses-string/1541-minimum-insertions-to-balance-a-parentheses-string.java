828b5bf3db2607b994aface9384fdd74cfa241f9
Atharva-001-cyber <atharvranjans65@gmail.com>
2026-08-07 11:24:29 +0530

    Time: 11 ms (63.18%), Space: 47.4 MB (77.73%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,29 +1,23 @@
 class Solution {
     public int minInsertions(String s) {
-        int ans = 0;
-        int need = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch == '(') {
-                need += 2;
-
-                if (need % 2 == 1) {
-                    ans++;
-                    need--;
-                }
-            } else {
+      int ans = 0;
+      int need = 0;
+      for(int i = 0; i < s.length(); i++){
+        char ch = s.charAt(i);
+        if(ch == '('){
+            need += 2;
+            if(need%2 == 1){
+                ans++;
                 need--;
-
-                if (need == -1) {
-                    ans++;
-                    need = 1;
-                }
             }
+        }else{
+            need--;
+        }if(need == -1){
+            ans++;
+            need = 1;
         }
-
-        return ans + need;
+      }
+      return ans + need;
     }
 }
 
